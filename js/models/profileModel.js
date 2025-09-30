75dcc882d13704e83dc95bccd741820e13e97bc7
DogSoulDev <dogsouldev@protonmail.com>
2025-09-30 21:45:59 +0200

    refactor

--- js/models/profileModel.js
+++ js/models/profileModel.js
@@ -6,11 +6,11 @@ export const ProfileModel = {
     title: 'Manga Web Developer & Mangaka',
     subtitle: 'Arte japonés, interfaces dibujadas a mano y creatividad digital',
     description: 'Mangaka y desarrollador web especializado en portafolios manga, interfaces ilustradas y creatividad digital. Apasionado por el arte japonés, la tecnología y el diseño dibujado a mano. Siempre explorando nuevas técnicas para crear y proteger.',
-    img: 'img/profile.jpg', // Avatar manga ficticio
+    img: 'img/avatar-manga.svg', // Avatar manga dibujado a mano
       social: [
-        { name: 'GitHub', url: 'https://github.com/akirayamada', icon: 'icons/python.svg' },
-        { name: 'LinkedIn', url: 'https://linkedin.com/in/akirayamada', icon: 'icons/java.svg' },
-        { name: 'Portfolio', url: 'https://akirayamada.dev', icon: 'icons/html.svg' }
+        { name: 'GitHub', url: 'https://github.com/akirayamada', icon: 'icons/manga-skill.svg' },
+        { name: 'LinkedIn', url: 'https://linkedin.com/in/akirayamada', icon: 'icons/manga-skill.svg' },
+        { name: 'Portfolio', url: 'https://akirayamada.dev', icon: 'icons/manga-skill.svg' }
       ]
     };
   }
